3444118e958652295e25a560b2e736892dc6aa43
glebshcherbakov <gleb.sher309@gmail.com>
2023-07-27 12:54:29 +0300

    solution hw2

---
+++ hw2/src/task4.java
@@ -0,0 +1,23 @@
+// Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. 
+// Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args) {
+        emptyString();
+    }
+
+    private static void emptyString() {
+        System.out.println("Введите текст: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        if (string.equals("")) {
+            throw new RuntimeException("Пустые строки вводить нельзя!");
+
+        }
+        System.out.println("Вы ввели: " + string);
+        scanner.close();
+    }
+}
